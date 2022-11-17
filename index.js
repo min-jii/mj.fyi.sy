03cd6264c1fe2546207f36ef647f7b3e5c13f6d8
min-jii <98052kmg@gmail.com>
2022-11-17 16:33:39 +0900

    add

---
+++ index.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+    $('.button1').click(function() {
+      $('.menu1').toggle("slide");
+    });
+
+    $('.button2').click(function() {
+      $('.menu2').toggle("slide");
+    });
+
+    $('.button3').click(function() {
+      $('.menu3').toggle("slide");
+    });
+
+    $('.button4').click(function() {
+      $('.menu4').toggle("slide");
+    });
+
+    $('.button5').click(function() {
+      $('.menu5').toggle("slide");
+    });
+
+    $('.button6').click(function() {
+      $('.menu6').toggle("slide");
+    });
+
+    $('.button7').click(function() {
+      $('.menu7').toggle("slide");
+    });
+
+    $('.button8').click(function() {
+      $('.menu8').toggle("slide");
+    });   
+
+    $('.button9').click(function() {
+      $('.menu9').toggle("slide");
+    });  
+
+    $('.button10').click(function() {
+      $('.menu10').toggle("slide");
+    });  
+
+    $('.button11').click(function() {
+      $('.menu11').toggle("slide");
+    });  
+
+    $('.button12').click(function() {
+      $('.menu12').toggle("slide");
+    });  
+
+    $('.button13').click(function() {
+      $('.menu13').toggle("slide");
+    });   
+
+    $('.button14').click(function() {
+      $('.menu14').toggle("slide");
+    }); 
+
+    $('.button15').click(function() {
+      $('.menu15').toggle("slide");
+    });                       
+});
